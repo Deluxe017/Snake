c0a65fa4d3a7caf4cb852b199b41c12d1b767702
unknown <SENA@MEDAPRCCSFSD338.FormacionSena.RED>
2022-07-21 14:57:23 -0500

    Movimiento

--- Snake_Project/Assets/Scripts/Movimiento.cs
+++ Snake_Project/Assets/Scripts/Movimiento.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float frameRate = 0.2f;
+    public float step = 0.16f;
+    enum Direction
     {
-        
+        up,
+        down,
+        left,
+        right,
     }
 
-    // Update is called once per frame
+    Direction direction;
+
+    
+    void Start()
+    {
+        InvokeRepeating("Move",frameRate,frameRate);
+    }
+
+    void Move()
+    {
+        Vector3 nextPos = Vector3.zero;
+        if (direction == Direction.up)
+            nextPos = Vector3.up;
+        else if (direction == Direction.down)
+            nextPos = Vector3.down;
+        else if (direction == Direction.left)
+            nextPos = Vector3.left;
+        else if (direction == Direction.right)
+            nextPos = Vector3.right;
+        nextPos *= step;
+        transform.position += nextPos;
+
+    }
+
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            direction = Direction.up;
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            direction = Direction.down;
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            direction = Direction.left;
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            direction = Direction.right;
     }
 }
